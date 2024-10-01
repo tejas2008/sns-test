3ea3e7aa1979e24044538a31dff9fea2a4ce4f71
Tejas Khanolkar <tejas.khanolkar@somaiya.edu>
2024-10-01 15:03:56 -0700

    up

--- amplify/data/resource.ts
+++ amplify/data/resource.ts
@@ -3,14 +3,13 @@ import { type ClientSchema, a, defineData } from '@aws-amplify/backend';
 import { schema as generatedSqlSchema } from './schema.sql';
 const sqlSchema = generatedSqlSchema.authorization(allow => allow.guest())
 
-
 const schema = a.schema({
   Todo: a
     .model({
       content: a.string(),
-      date: a.string(),
+      date: a.string()
     })
-    .authorization((allow) => [allow.guest()]),
+    .authorization((allow) => [allow.guest()])
 });
 
 const combinedSchema = a.combine([schema, sqlSchema]);
